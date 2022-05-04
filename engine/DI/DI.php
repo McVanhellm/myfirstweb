24e94c3f56ec2d33b1656f4ac4ba968e724cbf18
McVanhellm <mcvanhellm@gmail.com>
2022-05-04 19:52:41 +0300

    innition commit

--- engine/DI/DI.php
+++ engine/DI/DI.php
@@ -8,11 +8,37 @@ class DI {
     * @var array
     */
     private $container = [];
-    
+    /**
+     * set
+     *
+     * @param  mixed $key
+     * @param  mixed $value
+     * @return void
+     */
     public function set($key, $value){
     
     $this ->container[$key]=$value;
     
     return $this;
+    }
+    /**
+     * get
+     *
+     * @param  mixed $key
+     * @return void
+     */
+    public function get($key)
+    {
+        return $this-> has($key);
+    }    
+    /**
+     * has
+     *
+     * @param  mixed $key
+     * @return void
+     */
+    public function has($key){
+
+        return isset($this->container[$key]);
     }
 }
